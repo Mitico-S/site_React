2c6c8845b5b8f90ac7bb473f74bb52b1c2fca93a
João Pedro Alves <jotalmeida007@hotmail.com>
2020-05-04 08:35:43 -0300

    beautification

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import logoImg from '../../assets/logo.svg'
+import logoImg from '../../assets/logo.svg';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
 
 import './style.css';
 export default function Register() {
@@ -11,12 +13,26 @@ export default function Register() {
 
             <h1>Cadastro</h1>
             <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
-            
+            <Link className="back-link" to="/">
+                    <FiArrowLeft size={16} color="#e02041"/>
+                    Não tenho cadastro
+                </Link>
+
               </section>
             <form>
+                <input placeholder="Nome da ONG" />
+                <input type="email" placeholder="Email" />
+                <input placeholder="WhatsApp" placeholder="WhastApp" />
+
+                <div className="input-group">
+                  <input placeholder="Cidade" />
+                  <input placeholder="UF" style={{ width: 80 }} />
+                </div>
+
+                <button className="button" type="submit">Cadastrar</button>
 
             </form>
         </div>
       </div>
     );
-}
+}  
